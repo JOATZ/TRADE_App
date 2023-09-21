36dbc395216ecbeeb164a4333d4e41ab7e41d083
JOATZ <136692977+JOATZ@users.noreply.github.com>
2023-09-21 09:37:19 +0300

    T&C required setup

--- src/components/TermsModal.js
+++ src/components/TermsModal.js
@@ -3,6 +3,7 @@ import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'
 
 const TermsModal = () => {
     const [showTermsModal, setShowTermsModal] = useState(false)
+    const [userClosedModal, setUserClosedModal] = useState(false)
 
     useEffect(() => {
         const acceptedTerms = localStorage.getItem('acceptedTerms')
@@ -12,22 +13,69 @@ const TermsModal = () => {
     }, [])
 
     const handleTermsModalClose = () => {
-        localStorage.setItem('acceptedTerms', true)
+        setUserClosedModal(true)
         setShowTermsModal(false)
     }
 
+    const handleAcceptTerms = () => {
+        localStorage.setItem('acceptedTerms', true)
+        setUserClosedModal(false)
+        setShowTermsModal(false)
+    }
+
+    const reopenModal = () => {
+        setShowTermsModal(true)
+    }
+
     return (
-        <Modal isOpen={showTermsModal} toggle={handleTermsModalClose}>
-            <ModalHeader toggle={handleTermsModalClose}>
-                Terms and Conditions
-            </ModalHeader>
-            <ModalBody>Please accept our terms and conditions...</ModalBody>
-            <ModalFooter>
-                <Button color='primary' onClick={handleTermsModalClose}>
-                    I Accept
-                </Button>
-            </ModalFooter>
-        </Modal>
+        <>
+            <Modal
+                className='terms-modal'
+                isOpen={showTermsModal}
+                toggle={handleTermsModalClose}
+                backdrop='static'
+            >
+                <ModalHeader
+                    className='terms-modal-header'
+                    toggle={handleTermsModalClose}
+                >
+                    Terms and Conditions
+                </ModalHeader>
+                <ModalBody>
+                    By using this site, you agree to the following Terms and
+                    Conditions:
+                    <ol>
+                        <li>
+                            You are responsible for any data you upload to this
+                            site.
+                        </li>
+                        <li>
+                            We are not responsible for any data uploaded to this
+                            site.
+                        </li>
+                        <li>
+                            You agree not to upload any illegal or harmful data.
+                            Please use this site responsibly.
+                        </li>
+                    </ol>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color='primary' onClick={handleAcceptTerms}>
+                        I Accept
+                    </Button>
+                </ModalFooter>
+            </Modal>
+            {userClosedModal && !showTermsModal && (
+                <div className='terms-required'>
+                    <p>
+                        You are required to accept the terms to use this site.
+                    </p>
+                    <Button color='btn btn-primary' onClick={reopenModal}>
+                        Accept Terms
+                    </Button>
+                </div>
+            )}
+        </>
     )
 }
 
