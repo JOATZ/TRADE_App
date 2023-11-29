133d9b47523c74e21f075239ceb9476a2c312046
JOATZ <136692977+JOATZ@users.noreply.github.com>
2023-11-29 18:36:34 +0200

    button style override done

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import HomeButton from 'components/HomeButton'
-import SubHeader from 'components/SubHeader'
-import { Col, Container, Row } from 'reactstrap'
+import React from 'react'
+import { AwesomeButton } from 'react-awesome-button'
+import { NavLink } from 'react-router-dom'
+import { Container } from 'reactstrap'
+
+import 'react-awesome-button/dist/styles.css'
 
 const HomePage = () => {
     return (
         <Container fluid>
-            <HomeButton path='/datamanger' />
+            <NavLink className='nav-link' to='/datamanager'>
+                <AwesomeButton type='primary' style={buttonStyles}>
+                    Data Manager
+                </AwesomeButton>
+            </NavLink>
         </Container>
     )
+}
+//AwesomeButtonStyleOverrides
+const buttonStyles = {
+    '--button-default-height': '100px',
+    '--button-default-font-size': '20px'
 }
 
 export default HomePage
