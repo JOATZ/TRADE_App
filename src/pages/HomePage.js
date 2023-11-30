424d9f1fff1b52b63a5c6c8d538ca5531cc11d6d
JOATZ <136692977+JOATZ@users.noreply.github.com>
2023-11-30 12:07:10 +0200

    Layout Updates

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,19 +6,22 @@ import { Container, Row } from 'reactstrap'
 
 const pageLinks = [
     { title: 'Data Manager', to: '/datamanager', icon: <BsDatabaseFillGear /> },
-    { title: 'Title 2', to: '/datamanager', icon: <BsDatabaseFillGear /> },
+    { title: 'A very', to: '/datamanager', icon: <BsDatabaseFillGear /> },
     { title: 'Title 3', to: '/datamanager', icon: <BsDatabaseFillGear /> },
-    { title: 'Title 1', to: '/datamanager', icon: <BsDatabaseFillGear /> },
-    { title: 'Title 2', to: '/datamanager', icon: <BsDatabaseFillGear /> },
-    { title: 'Title 3', to: '/datamanager', icon: <BsDatabaseFillGear /> },
-    { title: 'Title 1', to: '/datamanager', icon: <BsDatabaseFillGear /> },
-    { title: 'Title 2', to: '/datamanager', icon: <BsDatabaseFillGear /> },
-    { title: 'Title 3', to: '/datamanager', icon: <BsDatabaseFillGear /> }
+    { title: 'Title 4', to: '/datamanager', icon: <BsDatabaseFillGear /> },
+    { title: 'Title 5', to: '/datamanager', icon: <BsDatabaseFillGear /> },
+    { title: 'Title 6', to: '/datamanager', icon: <BsDatabaseFillGear /> },
+    { title: 'Title 7', to: '/datamanager', icon: <BsDatabaseFillGear /> },
+    { title: 'Title 8', to: '/datamanager', icon: <BsDatabaseFillGear /> }
 ]
 
 const HomePage = () => {
     return (
-        <Container fluid className='home-page-container'>
+        <Container
+            fluid
+            className='home-page-container'
+            style={{ overflowY: 'scroll' }}
+        >
             <Row className='nav-btn-row'>
                 {pageLinks.map((page, index) => (
                     <NavLink
