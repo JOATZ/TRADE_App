e8f96efe367416f7f59da7a2d09f718708f588d8
JOATZ <136692977+JOATZ@users.noreply.github.com>
2023-11-30 11:05:20 +0200

    converted HomePage to ArrayMap

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,91 +5,31 @@ import { NavLink } from 'react-router-dom'
 import { Container, Row } from 'reactstrap'
 
 const pageLinks = [
-    { title: 'Title 1', to: '/path1', icon: <BsDatabaseFillGear /> },
-    { title: 'Title 2', to: '/path2', icon: <BsDatabaseFillGear /> },
-    { title: 'Title 3', to: '/path3', icon: <BsDatabaseFillGear /> }
+    { title: 'Data Manager', to: '/datamanager', icon: <BsDatabaseFillGear /> },
+    { title: 'Title 2', to: '/datamanager', icon: <BsDatabaseFillGear /> },
+    { title: 'Title 3', to: '/datamanager', icon: <BsDatabaseFillGear /> },
+    { title: 'Title 1', to: '/datamanager', icon: <BsDatabaseFillGear /> },
+    { title: 'Title 2', to: '/datamanager', icon: <BsDatabaseFillGear /> },
+    { title: 'Title 3', to: '/datamanager', icon: <BsDatabaseFillGear /> },
+    { title: 'Title 1', to: '/datamanager', icon: <BsDatabaseFillGear /> },
+    { title: 'Title 2', to: '/datamanager', icon: <BsDatabaseFillGear /> },
+    { title: 'Title 3', to: '/datamanager', icon: <BsDatabaseFillGear /> }
 ]
+
 const HomePage = () => {
     return (
         <Container fluid className='home-page-container'>
             <Row className='nav-btn-row'>
-                <NavLink
-                    style={navStyles.navLink}
-                    className='nav-link col-6 col-md-3'
-                    to='/datamanager'
-                >
-                    <HomePageTile
-                        title='Data Manager'
-                        icon={<BsDatabaseFillGear />}
-                    />
-                </NavLink>
-                <NavLink
-                    className='nav-link col-6 col-md-3'
-                    style={navStyles.navLink}
-                    to='/datamanager'
-                >
-                    <HomePageTile title='Chart' icon={<BsDatabaseFillGear />} />
-                </NavLink>
-                <NavLink
-                    className='nav-link col-6 col-md-3'
-                    style={navStyles.navLink}
-                    to='/datamanager'
-                >
-                    <HomePageTile
-                        title='Profile'
-                        icon={<BsDatabaseFillGear />}
-                    />
-                </NavLink>
-                <NavLink
-                    className='nav-link col-6 col-md-3'
-                    style={navStyles.navLink}
-                    to='/datamanager'
-                >
-                    <HomePageTile
-                        title='Page 4'
-                        icon={<BsDatabaseFillGear />}
-                    />
-                </NavLink>
-                <NavLink
-                    className='nav-link col-6 col-md-3'
-                    style={navStyles.navLink}
-                    to='/datamanager'
-                >
-                    <HomePageTile
-                        title='Page 5'
-                        icon={<BsDatabaseFillGear />}
-                    />
-                </NavLink>
-                <NavLink
-                    className='nav-link col-6 col-md-3'
-                    style={navStyles.navLink}
-                    to='/datamanager'
-                >
-                    <HomePageTile
-                        title='Page 6'
-                        icon={<BsDatabaseFillGear />}
-                    />
-                </NavLink>
-                <NavLink
-                    className='nav-link col-6 col-md-3'
-                    style={navStyles.navLink}
-                    to='/datamanager'
-                >
-                    <HomePageTile
-                        title='Page 7'
-                        icon={<BsDatabaseFillGear />}
-                    />
-                </NavLink>
-                <NavLink
-                    className='nav-link col-6 col-md-3'
-                    style={navStyles.navLink}
-                    to='/datamanager'
-                >
-                    <HomePageTile
-                        title='Page 8'
-                        icon={<BsDatabaseFillGear />}
-                    />
-                </NavLink>
+                {pageLinks.map((page, index) => (
+                    <NavLink
+                        key={index}
+                        style={navStyles.navLink}
+                        className='nav-link col-6 col-md-3'
+                        to={page.to}
+                    >
+                        <HomePageTile title={page.title} icon={page.icon} />
+                    </NavLink>
+                ))}
             </Row>
         </Container>
     )
