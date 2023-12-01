999d5d43676d76c4274ca9b37ed0a1c6bf0f0f7b
JOATZ <136692977+JOATZ@users.noreply.github.com>
2023-12-01 14:53:36 +0200

    Home Page Tiles DONE!!

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,35 @@
 import React from 'react'
-import { BsDatabaseFillGear } from 'react-icons/bs'
+import {
+    Bs2CircleFill,
+    BsAlarmFill,
+    BsArrowLeftRight,
+    BsBadgeHdFill,
+    BsDatabaseFillGear,
+    BsJournalRichtext
+} from 'react-icons/bs'
+import {} from 'react-icons/bs'
+import { FaMagnifyingGlassChart } from 'react-icons/fa6'
+import { RiStockFill } from 'react-icons/ri'
 import { NavLink } from 'react-router-dom'
-import { Container, Row } from 'reactstrap'
+import { Button, Container } from 'reactstrap'
 
 const pageLinks = [
     { title: 'Data Manager', to: '/datamanager', icon: <BsDatabaseFillGear /> },
-    { title: 'A very', to: '/datamanager', icon: <BsDatabaseFillGear /> },
-    { title: 'Title 3', to: '/datamanager', icon: <BsDatabaseFillGear /> },
-    { title: 'Title 4', to: '/datamanager', icon: <BsDatabaseFillGear /> },
-    { title: 'Title 5', to: '/datamanager', icon: <BsDatabaseFillGear /> },
-    { title: 'Title 6', to: '/datamanager', icon: <BsDatabaseFillGear /> },
-    { title: 'Title 7', to: '/datamanager', icon: <BsDatabaseFillGear /> },
-    { title: 'Title 8', to: '/datamanager', icon: <BsDatabaseFillGear /> }
+    { title: 'Chart Data', to: '/datamanager', icon: <RiStockFill /> },
+    { title: 'Trade Log', to: '/datamanager', icon: <BsJournalRichtext /> },
+    {
+        title: 'Trade Analysis',
+        to: '/datamanager',
+        icon: <FaMagnifyingGlassChart />
+    },
+    {
+        title: 'Title 5',
+        to: '/datamanager',
+        icon: <BsAlarmFill />
+    },
+    { title: 'Title 6', to: '/datamanager', icon: <BsArrowLeftRight /> },
+    { title: 'Title 7', to: '/datamanager', icon: <BsBadgeHdFill /> },
+    { title: 'Title 8', to: '/datamanager', icon: <Bs2CircleFill /> }
 ]
 
 const HomePage = () => {
@@ -19,8 +37,8 @@ const HomePage = () => {
         <Container fluid className='home-page-container'>
             {pageLinks.map((page, index) => (
                 <NavLink key={index} className='home-page-tile' to={page.to}>
-                    <div className='title'>{page.title}</div>
-                    <div className='icon'>{page.icon}</div>
+                    <div className='title-box'>{page.title}</div>
+                    <div className='icon-box'>{page.icon}</div>
                 </NavLink>
             ))}
         </Container>
