0d70cc0bfcecb36e82ffe73c6e7ab258e35e4914
JOATZ <136692977+JOATZ@users.noreply.github.com>
2023-12-07 16:22:20 +0200

    server.js works for lg files but slow

--- json-server/server.js
+++ json-server/server.js
@@ -1,11 +1,21 @@
 const jsonServer = require('json-server')
 const bodyParser = require('body-parser')
-const fs = require('fs')
 const multer = require('multer')
-const upload = multer() // for parsing multipart/form-data
+const path = require('path')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage }) // for parsing multipart/form-data
 
 const server = jsonServer.create()
-const router = jsonServer.router('db.json') // replace 'db.json' with your JSON file
+const router = jsonServer.router(path.join(__dirname, 'db.json')) // replace 'db.json' with your JSON file
 
 // Increase the limit
 server.use(bodyParser.json({ limit: '50mb' }))
@@ -13,33 +23,30 @@ server.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 
 // Handle file upload
 server.post('/upload', upload.single('file'), (req, res) => {
-    const writeStream = fs.createWriteStream('./uploadedFile')
-
-    req.file.stream.pipe(writeStream)
-
-    writeStream.on('finish', () => {
-        res.send('File uploaded successfully')
-    })
+    console.log(
+        `File received: ${req.file.originalname}, size: ${req.file.size} bytes`
+    )
+    res.send('File uploaded successfully')
 })
 
 server.use(jsonServer.defaults())
-server.use(router)
 
 // Specify the port and delay
 const port = 3001
-const delay = 50
+const delay = 0
+
 server.use(
     jsonServer.rewriter({
         '/api/*': '/$1'
     })
 )
-server.use(jsonServer.defaults())
-server.use(jsonServer.bodyParser)
+
 server.use((req, res, next) => {
     setTimeout(next, delay)
 })
+
 server.use(router)
 
-server.listen(port, () => {
-    console.log(`JSON Server is running on port ${port}`)
+server.listen(port, '0.0.0.0', () => {
+    console.log(`JSON Server is running on http://0.0.0.0:${port}`)
 })
