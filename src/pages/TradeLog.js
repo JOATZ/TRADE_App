14352b25dc3c53eb2c8221c2acb8c75887b95803
JOATZ <136692977+JOATZ@users.noreply.github.com>
2023-12-08 21:13:03 +0200

    layout refinements

--- src/pages/TradeLog.js
+++ src/pages/TradeLog.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { MdConstruction } from 'react-icons/md'
 import { Container } from 'reactstrap'
 
 const TradeLog = () => {
     return (
-        <Container fluid className='trade-log-container'>
-            Some text
+        <Container
+            fluid
+            className='trade-log-container'
+            style={{
+                color: 'var(--color-accent)',
+                background: 'var(--color-background)'
+            }}
+        >
+            <MdConstruction style={{ height: '50vh', width: '100vw' }} />
+            Working on it!
         </Container>
     )
 }
