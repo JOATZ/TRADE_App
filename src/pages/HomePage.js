d57e649e668eb0da6143d838daf696ddb54f6e46
JOATZ <136692977+JOATZ@users.noreply.github.com>
2023-12-13 15:24:50 +0200

    TermsAccepted Fixed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { isTermsAccepted } from 'features/termsConditions/termsSlice'
 import {
     Bs2CircleFill,
     BsAlarmFill,
@@ -9,6 +10,7 @@ import {
 } from 'react-icons/bs'
 import { FaMagnifyingGlassChart } from 'react-icons/fa6'
 import { RiStockFill } from 'react-icons/ri'
+import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import { Container } from 'reactstrap'
 
@@ -32,16 +34,23 @@ const pageLinks = [
 ]
 
 const HomePage = () => {
-    return (
-        <Container fluid className='home-page-container'>
-            {pageLinks.map((page, index) => (
-                <NavLink key={index} className='home-page-tile' to={page.to}>
-                    <div className='title-box'>{page.title}</div>
-                    <div className='icon-box'>{page.icon}</div>
-                </NavLink>
-            ))}
-        </Container>
-    )
+    const termsAccepted = useSelector(isTermsAccepted)
+    if (termsAccepted) {
+        return (
+            <Container fluid className='home-page-container'>
+                {pageLinks.map((page, index) => (
+                    <NavLink
+                        key={index}
+                        className='home-page-tile'
+                        to={page.to}
+                    >
+                        <div className='title-box'>{page.title}</div>
+                        <div className='icon-box'>{page.icon}</div>
+                    </NavLink>
+                ))}
+            </Container>
+        )
+    }
 }
 
 export default HomePage
